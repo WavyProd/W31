bdd682a0d9fbfddf1c1c15fd714cbfe20ea5ed8d
June76 <98433447+WavyProd@users.noreply.github.com>
2022-03-11 00:43:54 +0000

    Arith Final

--- arith.py
+++ arith.py
@@ -1,252 +1,107 @@
-INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = (
-    'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF'
-)
+import sys
 
 
-class Token(object):
-    def __init__(self, type, value):
-        self.type = type
-        self.value = value
-
-    def __str__(self):
-        """String representation of the class instance.
-
-        Examples:
-            Token(INTEGER, 3)
-            Token(PLUS, '+')
-            Token(MUL, '*')
-        """
-        return 'Token({type}, {value})'.format(
-            type=self.type,
-            value=repr(self.value)
-        )
-
-    def __repr__(self):
-        return self.__str__()
+def parser(expr):
+    length = len(expr)
+    elements = expr.split()
+    if length < 3:
+        print("Error: cannot parser the statement")
+    else:
+        return statement_parser(elements)
 
 
-class Lexer(object):
-    def __init__(self, text):
-        # client string input, e.g. "4 + 2 * 3 - 6 / 2"
-        self.text = text
-        # self.pos is an index into self.text
-        self.pos = 0
-        self.current_char = self.text[self.pos]
-
-    def error(self):
-        raise Exception('Invalid character')
-
-    def advance(self):
-        """Advance the `pos` pointer and set the `current_char` variable."""
-        self.pos += 1
-        if self.pos > len(self.text) - 1:
-            self.current_char = None  # Indicates end of input
+def statement_parser(elements):
+    operator = {'+', '-', '*', '/', '**'}
+    try:
+        if len(elements) == 0:
+            return []
+        elif len(elements) == 1:
+            return elements[0]
+        elif elements[1] in operator:
+            if len(elements) < 3:
+                raise SyntaxError(f"Missing values for {elements[1]}")
+            # check if the preceding or succeeding element are valid numbers
+            if invalid_number(elements[0]) or invalid_number(elements[2]):
+                raise SyntaxError(f"Invalid syntax: {elements[0]} {elements[1]} {elements[2]}")
+            if elements[1] in {'+', '-'}:  # [1, +, []]
+                return [elements[0], elements[1], statement_parser(elements[2:])]
+            elif elements[1] in {'*', '/'}:  # [[1, *, 2], ...]
+                # check if a exp is followed
+                if 4 <= len(elements) and elements[3] == '**':
+                    # check if there are enough terms
+                    if len(elements) < 5:
+                        raise SyntaxError(f"Invalid syntax: missing values {elements[4]}")
+                    # check if the value followed is valid
+                    if invalid_number(elements[4]):
+                        raise SyntaxError(f"Invalid syntax: {' '.join(elements[2: 5])}")
+                    exp = [elements[0], elements[1], elements[2:5]]
+                    exp.extend(elements[5:])
+                    return statement_parser(exp)
+                else:
+                    product = [elements[0:3]]
+                    product.extend(elements[3:])
+                    return statement_parser(product)
+            elif elements[1] == '**':
+                exp = [elements[0:3]]
+                exp.extend(elements[3:])
+                return statement_parser(exp)
         else:
-            self.current_char = self.text[self.pos]
-
-    def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
-
-    def integer(self):
-        """Return a (multidigit) integer consumed from the input."""
-        result = ''
-        while self.current_char is not None and self.current_char.isdigit():
-            result += self.current_char
-            self.advance()
-        return int(result)
-
-    def get_next_token(self):
-        """Lexical analyzer (also known as scanner or tokenizer)
-
-        This method is responsible for breaking a sentence
-        apart into tokens. One token at a time.
-        """
-        while self.current_char is not None:
-
-            if self.current_char.isspace():
-                self.skip_whitespace()
-                continue
-
-            if self.current_char.isdigit():
-                return Token(INTEGER, self.integer())
-
-            if self.current_char == '+':
-                self.advance()
-                return Token(PLUS, '+')
-
-            if self.current_char == '-':
-                self.advance()
-                return Token(MINUS, '-')
-
-            if self.current_char == '*':
-                self.advance()
-                return Token(MUL, '*')
-
-            if self.current_char == '/':
-                self.advance()
-                return Token(DIV, '/')
-
-            if self.current_char == '(':
-                self.advance()
-                return Token(LPAREN, '(')
-
-            if self.current_char == ')':
-                self.advance()
-                return Token(RPAREN, ')')
-
-            self.error()
-
-        return Token(EOF, None)
+            raise SyntaxError(f"Invalid syntax: {elements[0]}  {elements[1]}")
+    except SyntaxError:
+        raise
 
 
-###############################################################################
-#                                                                             #
-#  PARSER                                                                     #
-#                                                                             #
-###############################################################################
-
-class AST(object):
-    pass
-
-
-class BinOp(AST):
-    def __init__(self, left, op, right):
-        self.left = left
-        self.token = self.op = op
-        self.right = right
-
-
-class Num(AST):
-    def __init__(self, token):
-        self.token = token
-        self.value = token.value
-
-
-class Parser(object):
-    def __init__(self, lexer):
-        self.lexer = lexer
-        # set current token to the first token taken from the input
-        self.current_token = self.lexer.get_next_token()
-
-    def error(self):
-        raise Exception('Invalid syntax')
-
-    def eat(self, token_type):
-        # compare the current token type with the passed token
-        # type and if they match then "eat" the current token
-        # and assign the next token to the self.current_token,
-        # otherwise raise an exception.
-        if self.current_token.type == token_type:
-            self.current_token = self.lexer.get_next_token()
+def invalid_number(element):
+    if type(element) == list:
+        return False
+    operator = {'+', '-'}
+    if not element.isnumeric():
+        if element[0] in operator and element[1:].isnumeric():
+            return False
         else:
-            self.error()
-
-    def factor(self):
-        """factor : INTEGER | LPAREN expr RPAREN"""
-        token = self.current_token
-        if token.type == INTEGER:
-            self.eat(INTEGER)
-            return Num(token)
-        elif token.type == LPAREN:
-            self.eat(LPAREN)
-            node = self.expr()
-            self.eat(RPAREN)
-            return node
-
-    def term(self):
-        """term : factor ((MUL | DIV) factor)*"""
-        node = self.factor()
-
-        while self.current_token.type in (MUL, DIV):
-            token = self.current_token
-            if token.type == MUL:
-                self.eat(MUL)
-            elif token.type == DIV:
-                self.eat(DIV)
-
-            node = BinOp(left=node, op=token, right=self.factor())
-
-        return node
-
-    def expr(self):
-        """
-        expr   : term ((PLUS | MINUS) term)*
-        term   : factor ((MUL | DIV) factor)*
-        factor : INTEGER | LPAREN expr RPAREN
-        """
-        node = self.term()
-
-        while self.current_token.type in (PLUS, MINUS):
-            token = self.current_token
-            if token.type == PLUS:
-                self.eat(PLUS)
-            elif token.type == MINUS:
-                self.eat(MINUS)
-
-            node = BinOp(left=node, op=token, right=self.term())
-
-        return node
-
-    def parse(self):
-        return self.expr()
+            return True
+    return False
 
 
-###############################################################################
-#                                                                             #
-#  INTERPRETER                                                                #
-#                                                                             #
-###############################################################################
-
-class NodeVisitor(object):
-    def visit(self, node):
-        method_name = 'visit_' + type(node).__name__
-        visitor = getattr(self, method_name, self.generic_visit)
-        return visitor(node)
-
-    def generic_visit(self, node):
-        raise Exception('No visit_{} method'.format(type(node).__name__))
+def is_operator(element):
+    operator = {'+', '-', '*', '/', '**'}
+    if type(element) == str and element in operator:
+        return True
+    return False
 
 
-class Interpreter(NodeVisitor):
-    def __init__(self, parser):
-        self.parser = parser
+def eval(ast):
+    if type(ast[0]) == str and type(ast[2]) == str:
+        return calculate(int(ast[0]), ast[1], int(ast[2]))
+    elif type(ast[0]) == str and type(ast[2]) == list:
+        return calculate(int(ast[0]), ast[1], eval(ast[2]))
+    elif type(ast[0]) == list and type(ast[2]) == str:
+        return calculate(eval(ast[0]), ast[1], int(ast[2]))
+    elif type(ast[0]) == list and type(ast[2]) == list:
+        return calculate(eval(ast[0]), ast[1], eval(ast[2]))
+    else:
+        raise ValueError(ast)
 
-    def visit_BinOp(self, node):
-        if node.op.type == PLUS:
-            return self.visit(node.left) + self.visit(node.right)
-        elif node.op.type == MINUS:
-            return self.visit(node.left) - self.visit(node.right)
-        elif node.op.type == MUL:
-            return self.visit(node.left) * self.visit(node.right)
-        elif node.op.type == DIV:
-            return self.visit(node.left) / self.visit(node.right)
 
-    def visit_Num(self, node):
-        return node.value
-
-    def interpret(self):
-        tree = self.parser.parse()
-        return self.visit(tree)
+def calculate(num1, operator, num2):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
+    elif operator == '**':
+        return num1 ** num2
+    else:
+        raise ValueError(f"Invalid value: {operator}")
 
 
 def main():
-    while True:
-        try:
-            try:
-                text = raw_input('spi> ')
-            except NameError:  # Python3
-                text = input('spi> ')
-        except EOFError:
-            break
-        if not text:
-            continue
-
-        lexer = Lexer(text)
-        parser = Parser(lexer)
-        interpreter = Interpreter(parser)
-        result = interpreter.interpret()
-        print(result)
+    for line in sys.stdin:
+        print(eval(parser(line)))
+    # print(parser("2 * 2 ** 2"))
 
 
 if __name__ == '__main__':
